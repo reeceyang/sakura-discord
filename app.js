ba82fa00e4eb475d865fb6e3aeb9c7ff31a11a92
reeceyang <reeceyang@live.com>
2021-05-08 19:11:24 -0400

    fix sendMessages if statement

--- app.js
+++ app.js
@@ -105,14 +105,14 @@ setInterval(async () => {
     const studyChannel = client.channels.cache.find(ch => ch.name === 'study-room');
     // const connection = client.channels.cache.find(ch => ch.name === 'study-vc').join();
     if ((min == 5 || min == 35) && t.getSeconds() == 0 && !playedBefore) {
-      if (!sendMessages) studyChannel.send("break time over! work for 25 minutes now");
+      if (sendMessages) studyChannel.send("break time over! work for 25 minutes now");
       if (joinedVoiceChannel) {
         const connection = await voiceChannel.join();
         connection.play('https://reeceyang.github.io/sakura-timer/mixkit-phone-ring-bell-593.wav');
       }
       playedBefore = true;
     } else if ((min == 0 || min == 30) && t.getSeconds() == 0 && !playedBefore) {
-      if (!sendMessages) studyChannel.send("work time over! break for 5 minutes now");
+      if (sendMessages) studyChannel.send("work time over! break for 5 minutes now");
       if (joinedVoiceChannel) {
         const connection = await voiceChannel.join();
         connection.play('https://reeceyang.github.io/sakura-timer/mixkit-achievement-bell-600.wav');
